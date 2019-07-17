4491ee42c2fb05c65a6c8e4705269518338480bb
praveen kumar <41241820+mepky@users.noreply.github.com>
2019-07-17 22:15:49 +0530

    Add files via upload

---
+++ inorder_traversal.py
@@ -0,0 +1,30 @@
+class Node:
+	def __init__(self,data):
+		self.data=data
+		self.left=None
+		self.right=None
+
+def inorder(root):
+	current=root
+	stack=[]
+	d=0
+	while True:
+		if current is not None:
+			stack.append(current)
+			current=current.left
+		elif stack is None:
+			current=stack.pop()
+			print(current.data)
+			current=current.right
+		else:
+			break
+
+	print()
+
+root = Node(1) 
+root.left = Node(2) 
+root.right = Node(3) 
+root.left.left = Node(4) 
+root.left.right = Node(5) 
+  
+inorder(root)
