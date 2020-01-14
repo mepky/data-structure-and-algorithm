8fcf51cd602f23d9ff963b0ab83cac207616347c
praveen kumar <41241820+mepky@users.noreply.github.com>
2020-01-14 20:41:20 +0530

    Add files via upload

---
+++ Solution4.java
@@ -0,0 +1,25 @@
+package Function;
+
+import java.util.Scanner;
+public class Solution4 {
+
+    public static void main(String[] args) {
+        // Write your code here
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int i=1;
+
+        while(i*i<n){
+
+            i+=1;
+        }
+        System.out.print(i);
+
+
+
+
+
+
+
+    }
+}
