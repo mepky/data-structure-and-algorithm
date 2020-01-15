ebccc8aad4d5c71418525f3869a32560746c7cae
praveen kumar <41241820+mepky@users.noreply.github.com>
2020-01-15 21:43:38 +0530

    Add files via upload

--- Solution4.java
+++ Solution4.java
@@ -3,23 +3,11 @@ package Function;
 import java.util.Scanner;
 public class Solution4 {
 
-    public static void main(String[] args) {
-        // Write your code here
-        Scanner s=new Scanner(System.in);
-        int n=s.nextInt();
-        int i=1;
-
-        while(i*i<n){
-
-            i+=1;
-        }
-        System.out.print(i);
-
-
-
-
-
-
-
+    public static void main(String[] args)
+    {
+        int a=5,b=4;
+        System.out.println(a*=b);//line 1
+        System.out.println(a=(a==b));//line 2
+        System.out.println(a==b);//line 3
     }
 }
